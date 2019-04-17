bdafdc1d0274727f2ccf95d4d7f4c2ddaf21cb54
Caio Batista <caiobatis7@gmail.com>
2019-04-17 16:42:35 -0300

    new configs, add css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
+import styles from './app.scss'
+
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className="App">
+        <div className={styles.header}>
+          <div className={styles.navbar}></div>
+          <div className={styles.masthead}></div>
+        </div>
+        <div className={styles.steps}></div>
+        <div className={styles.services}></div>
+        <div className={styles.who}></div>
+        <div className={styles.win}></div>
+        <div className={styles.about}></div>
+        <div className={styles.socials}></div>
+        <div className={styles.vehicle}></div>
+        <div className={styles.contact}></div>
+        <div className={styles.footer}></div>
+      </div>
     )
   }
 }
