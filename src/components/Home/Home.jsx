7e54ee86d611e717294fb6a2abe9699bee615acc
Caio Batista <caiobatis7@gmail.com>
2019-05-02 19:25:48 -0300

    mobile styles changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -24,7 +24,16 @@ class Routes extends Component {
       centerPadding: 0,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+      ]
     }
   
     AsyncScriptLoader('https://www.powr.io/powr.js?platform=html')
--- src/components/Home/HomeChallenger.jsx
+++ src/components/Home/HomeChallenger.jsx
@@ -12,7 +12,16 @@ const HomeChallenger = props => {
     centerPadding: 0,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   }
 
   return (
